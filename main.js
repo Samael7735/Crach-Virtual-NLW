5e31a8aec83581da9777f122192a9cb0f3acfd4d
samael7735 <senasamuel7735@gmail.com>
2022-05-19 14:12:47 -0300

    Atualizações do projeto crachá virtual.

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMidia = {
+  github: 'Samael7735',
+  instagram: 'senasamuel639',
+  facebook: 'SamuelSena',
+  youtube: 'channel/UCLjV-jhntlhLhNnmlpMx1Lw'
+}
+function changeLinksSocialMidia() {
+  for (let li of SocialLinks.children) {
+    const social = li.getAttribute('class')
+    console.log(social)
+    li.children[0].href = `https://${social}.com/${LinksSocialMidia[social]}`
+  }
+}
+
+changeLinksSocialMidia();
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMidia.github}`
+
+  //alert(url)
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      UserName.textContent = data.name
+      UserBio.textContent = data.bio
+      UserLogin.textContent = data.login
+
+      UserPhoto.src = data.avatar_url
+    })
+}
+
+getGithubProfileInfos();
